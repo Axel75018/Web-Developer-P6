371aa0f12b5b60b806bce2fa27c09ea2cb49ba9a
Axel75018 <axel.calvet@gmail.com>
2023-04-19 10:47:42 +0200

    login sans DB

--- back/app.js
+++ back/app.js
@@ -1,24 +1,58 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-
+app.use(express.json());
+//header pour passer d'un server à l'autre
+//Cross Origin Resource Sharing mais ici on autorise tout
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  // PATCH, OPTIONS enlevé car pas présent dans le tableau
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+
+// je commence par le squelette du get route:/api/auces mais avec les bonnes routes
+app.get('/api/sauces', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue ici!' });
-  next();
+// ici je rajoute la route du sign up api/auth/signup
+// il me manque le hash pour l'instant
+app.post('/api/auth/signup', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
 });
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+// ici je rajoute la route du login api/auth/login
+// il me manque le token pour l'instant
+app.post('/api/auth/login', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
 });
 
 module.exports = app;
